0cb127c2b4009b8b45391bc04c763013293d08ad
Habibur137 <infernohabib@gmail.com>
2022-05-05 16:43:34 +0600

    get all data from database

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mdpo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const allInventoryItem = client
+      .db("sunriseMotors")
+      .collection("inventoryItem");
+    // get all data from db
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = allInventoryItem.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    console.log("connect mongo");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
