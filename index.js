76b5257d5768e1456c668e46dbbfbd6d36a377ae
Habibur137 <infernohabib@gmail.com>
2022-05-07 19:51:43 +0600

    server side done

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  next();
+});
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
